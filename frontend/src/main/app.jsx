82da990b2b5e14988a24ea4098dd6dc01c7bb430
LuizHonorato <lhehonorato@gmail.com>
2018-07-11 17:38:34 -0300

    Compra Certa

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import '../common/template/dependencies'
+
+import React from 'react'
+import ProductList from '../productsList/productList'
+
+export default props => (
+
+        <ProductList />
+
+)
